a0162e2b289bf9a02c54d79694eaa01fe6a10029
Patrick Blaas <patrick@kite4fun.nl>
2020-03-27 23:22:57 +0100

    update marked and minimist modules

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 import { express as voyagerMiddleware } from 'graphql-voyager/middleware';
-
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
   schema,
